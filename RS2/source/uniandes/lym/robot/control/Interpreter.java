69b5cda05b2d59937635ccf8b98a81903374971f
Nicolas <na.rodriguezp@uniandes.edu.co>
2020-02-01 10:16:58 -0500

    j

--- RS2/source/uniandes/lym/robot/control/Interpreter.java
+++ RS2/source/uniandes/lym/robot/control/Interpreter.java
@@ -15,21 +15,21 @@ import uniandes.lym.robot.kernel.*;
 
 public class Interpreter   
 {
-	
+
 	/**
 	 * HashMap that contains the variables
 	 */
 	private HashMap<String, Integer> hashVariables;
-	
+
 	/**
 	 * Robot's world
 	 */
 	private RobotWorldDec world;  
-	
+
 	// --------------------------------------------------------------------------
 	// Constants
 	// --------------------------------------------------------------------------
-	
+
 	/** 
 	 * Constants to model the directions	
 	 */
@@ -45,26 +45,26 @@ public class Interpreter
 	public static final String BACK = "BACK";
 	public static final String BALLOONS = "BALLOONS";
 	public static final String CHIPS = "CHIPS";
-	
-	
+
+
 	public Interpreter()
-	  {
-	  }
+	{
+	}
 
 
-    /**
+	/**
 	 * Creates a new interpreter for a given world
 	 * @param world 
 	 */
 
 
 	public Interpreter(RobotWorld mundo)
-      {
+	{
 		this.world =  (RobotWorldDec) mundo;
-		
-	  }
-	
-	
+
+	}
+
+
 	/**
 	 * sets a the world
 	 * @param world 
@@ -73,11 +73,11 @@ public class Interpreter
 	public void setWorld(RobotWorld m) 
 	{
 		world = (RobotWorldDec) m;
-		
+
 	}
-	  
-	
-	
+
+
+
 	/**
 	 *  Processes a sequence of commands. A command is a letter  followed by a ";"
 	 *  The command can be:
@@ -86,64 +86,64 @@ public class Interpreter
 	 *  
 	 * @param input Contiene una cadena de texto enviada para ser interpretada
 	 */
-	
+
 	public String process(String input) throws Error
-     {   
+	{   
 		
-		 
+
 		StringBuffer output=new StringBuffer("SYSTEM RESPONSE: -->\n");	
-		
+
 		int i;
-	   int n;
-	   boolean ok = true;
-	   n= input.length();
-	    
-	    i  = 0;
-	    try	    {
-	    while (i < n &&  ok) {
-	    	switch (input.charAt(i)) {
-	    		case 'M': world.moveForward(1); output.append("move \n");break;
-	    		case 'R': world.turnRight(); output.append("turnRignt \n");break;
-	    		case 'C': world.putChips(1); output.append("putChip \n");break;
-	    		case 'B': world.putBalloons(1); output.append("putBalloon \n");break;
-	    		case  'c': world.pickChips(1); output.append("getChip \n");break;
-	    		case  'b': world.grabBalloons(1); output.append("getBalloon \n");break;
-	    		default: output.append(" Unrecognized command:  "+ input.charAt(i)); ok=false;
-	          }
-	
-	    	  if (ok) {
-	    		 if  (i+1 == n)  { output.append("expected ';' ; found end of input; ");  ok = false ;}
-	    		 else if (input.charAt(i+1) == ';') 
-	    		 {
-	    			 i= i+2;
-	    			 try {
-	    			        Thread.sleep(1000);
-	    			    } catch (InterruptedException e) {
-	    			        System.err.format("IOException: %s%n", e);
-	    			    }
-	    			 
-	    		 }
-	    		 else {output.append(" Expecting ;  found: "+ input.charAt(i+1)); ok=false;
-	    	 }
-	    	 }
-	    
-	    
-	    }
-	    
-	    }
-	 catch (Error e ){
-	 output.append("Error!!!  "+e.getMessage());
-	 
- }
-	    return output.toString();
-	    }
-	
-	
-	
+		int n;
+		boolean ok = true;
+		n= input.length();
+
+		i  = 0;
+		try	    {
+			while (i < n &&  ok) {
+				switch (input.charAt(i)) {
+				case 'M': world.moveForward(1); output.append("move \n");break;
+				case 'R': world.turnRight(); output.append("turnRignt \n");break;
+				case 'C': world.putChips(1); output.append("putChip \n");break;
+				case 'B': world.putBalloons(1); output.append("putBalloon \n");break;
+				case  'c': world.pickChips(1); output.append("getChip \n");break;
+				case  'b': world.grabBalloons(1); output.append("getBalloon \n");break;
+				default: output.append(" Unrecognized command:  "+ input.charAt(i)); ok=false;
+				}
+
+				if (ok) {
+					if  (i+1 == n)  { output.append("expected ';' ; found end of input; ");  ok = false ;}
+					else if (input.charAt(i+1) == ';') 
+					{
+						i= i+2;
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							System.err.format("IOException: %s%n", e);
+						}
+
+					}
+					else {output.append(" Expecting ;  found: "+ input.charAt(i+1)); ok=false;
+					}
+				}
+
+
+			}
+
+		}
+		catch (Error e ){
+			output.append("Error!!!  "+e.getMessage());
+
+		}
+		return output.toString();
+	}
+
+
+
 	// --------------------------------------------------------------------------
 	// Proyect methods
 	// --------------------------------------------------------------------------
-	
+
 	/**
 	 * 
 	 * @param name
@@ -153,7 +153,7 @@ public class Interpreter
 	{
 		hashVariables.replace(name, n);
 	}
-	
+
 	/**
 	 * 
 	 */
@@ -162,15 +162,109 @@ public class Interpreter
 		int steps = Integer.parseInt(n);
 		world.moveForward(steps);
 	}
+
+	/**
+	 * 
+	 */
+	public void turn( String D )
+	{
+		if( D.equals(RIGHT) )
+		{
+			world.turnRight();
+		}
+		else if( D.equals(AROUND) )
+		{
+			world.turnRight();
+			world.turnRight();
+		}
+		else if( D.equals(LEFT) )
+		{
+			world.turnRight();
+			world.turnRight();
+			world.turnRight();
+		}
+	}
 	
 	/**
 	 * 
 	 */
-	public void turn()
+	public void face( int O )
 	{
-		
+		int orientation = world.getOrientacion();
+		if( O == 0 )
+		{
+			if( orientation == 1 )
+			{
+				world.turnRight();
+				world.turnRight();
+			}
+			else if( orientation == 2 )
+			{
+				world.turnRight();
+				world.turnRight();
+				world.turnRight();
+			}
+			else if( orientation == 3 )
+			{
+				world.turnRight();
+			}
+		}
+		else if( O == 1)
+		{
+			if( orientation == 0 )
+			{
+				world.turnRight();
+				world.turnRight();
+			}
+			else if( orientation == 2 )
+			{
+				world.turnRight();
+			}
+			else if( orientation ==3 )
+			{
+				world.turnRight();
+				world.turnRight();
+				world.turnRight();
+			}
+		}
+		else if( O == 2 )
+		{
+			if ( orientation == 0 )
+			{
+				world.turnRight();
+			}
+			else if( orientation == 1 )
+			{
+				world.turnRight();
+				world.turnRight();
+				world.turnRight();
+			}
+			else if( orientation == 3 )
+			{
+				world.turnRight();
+				world.turnRight();
+			}
+		}
+		else if( O == 3 )
+		{
+			if( orientation == 0 )
+			{
+				world.turnRight();
+				world.turnRight();
+				world.turnRight();
+			}
+			else if( orientation == 1 )
+			{
+				world.turnRight();
+			}
+			else if( orientation == 2 )
+			{
+				world.turnRight();
+				world.turnRight();
+			}
+		}
 	}
-
+	
+	
 	
 }
-	    
